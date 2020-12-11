5f7adfbcf82f7d57fc570e6bab2fe15ae9f8725b
murilovidal <murilovidal@gmail.com>
2020-12-11 16:33:35 -0300

    Tags for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,45 @@ class ArticlesController < ApplicationController
     def new
         unless authorized?
             @article = Article.new
+            @tags = Tag.all
         end
     end
 
+    def create
+        @article = Article.new()
+        
+        @article.title = article_params[:title]
+        @article.body = article_params[:body]
+
+        @tags = article_params[:tag]
+        @tags = @tags.gsub(/\s+/m, ' ').strip.split(" ")
+        
+        @tags.each do |tag|
+            if Tag.find_by(tag: tag)
+                @tag = Tag.find_by(tag: tag)
+                @article.tags << @tag
+            else
+                @article.tags.build(tag: tag)  
+            end
+        end
+
+        if @article.save
+            redirect_to '/article'
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.delete  
+        redirect_to articles_path      
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :body, :tag)
+    end
+    
+    
 end
